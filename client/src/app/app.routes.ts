6e27ad882864ec7b7cb99c9c1fda9b50ec390639
Onur DİKİLİTAŞ <75135735+onurdikilitas@users.noreply.github.com>
2025-06-01 02:59:24 +0300

    End of section 15
    Also lecture 192 outbox photo logic fixed.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { authGuard } from './_guards/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
 
 
 export const routes: Routes = [
@@ -17,7 +18,7 @@ export const routes: Routes = [
     path: '', runGuardsAndResolvers: 'always', canActivate: [authGuard],
     children: [
       { path: 'members', component: MemberListComponent, },
-      { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'members/:username', component: MemberDetailComponent, resolve: { member: memberDetailedResolver } },
       { path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard] },
       { path: 'lists', component: ListsComponent, },
       { path: 'messages', component: MessagesComponent, },
